c041276769240c218e900254efa54bb69c0f6851
hasifake <110405530+hasifake@users.noreply.github.com>
2022-08-01 21:15:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.owner = ['947779711041']
+global.premium = ['947779711041']
+global.ownernomer = '947779711041'
+global.ownername = 'MAD HASIYA'
+global.botname = 'HASIYA MD BOT'
+global.footer = 'HASIYA MD'
+global.cap = '*Genarated By Hasiya Md*'
 global.ig = 'https://github.com/vihangabot'
 global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
 global.sc = 'https://github.com/vihangabot'
-global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.myweb = 'https://www.facebook.com/Mafiyagaming24'
+global.packname = 'Hasiya Md Sticker Bot'
+global.author = 'MAD HASIYA'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
